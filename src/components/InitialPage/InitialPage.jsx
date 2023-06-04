b5545c67eefc40acbcf6ff288873dee57f976c3b
Fenickss <denis.suxov.1991@mail.ru>
2023-06-04 14:40:22 +0300

    add

--- src/components/InitialPage/InitialPage.jsx
+++ src/components/InitialPage/InitialPage.jsx
@@ -1,42 +1,52 @@
 import React from 'react';
-import style from './InitialPage.module.css';
+import s from './InitialPage.module.css';
+import frameImg from '../../image/Frame.jpg';
 
 const InitialPage = () => {
   return (
     <section className="section">
-      <div className={style.container}>
-        <div>
-          <img src="" alt="изображение управления кошельком" width="435px" />
-          <h1>Finance App</h1>
-        </div>
-        <div className="Finance__menu">
-          <p className="logo">Wallet</p>
-          <form className="form" action="">
-            <div className="form__field">
-              <input
-                className="form__input"
-                type="email"
-                name="email"
-                placeholder="E-mail"
+      <div className={s.container}>
+        <ul className={s.img__list}>
+          <li className={s.img__itemLeft}>
+            <div className={s.img__position}>
+              <img
+                className={s.img}
+                src={frameImg}
+                alt="изображение управления кошельком"
               />
-              <label className="form-label" htmlFor="email"></label>
+              <h1 className={s.img__title}>Finance App</h1>
             </div>
-            <div className="form__field">
-              <input
-                className="form__input"
-                type="password"
-                name="password"
-                placeholder="Пароль"
-              />
-              <label className="form-label" htmlFor="password"></label>
+          </li>
+          <li className={s.img__itemRight}>
+            <div className={s.finance__menu}>
+              <p className={s.logo}>Wallet</p>
+              <form className="form" action="">
+                <div className="form__field">
+                  <input
+                    className={s.form__input}
+                    type="email"
+                    name="email"
+                    placeholder="E-mail"
+                  />
+                  <label className="form-label" htmlFor="email"></label>
+                </div>
+                <div className={s.form__field}>
+                  <input
+                    className={s.form__input}
+                    type="password"
+                    name="password"
+                    placeholder="Пароль"
+                  />
+                  <label className="form-label" htmlFor="password"></label>
+                  <button className={s.button__entry}>Вход</button>
+                  <button className={s.button__registration}>
+                    Регистрация
+                  </button>
+                </div>
+              </form>
             </div>
-
-            <div className="form__button--position">
-              <button className="entry">Вход</button>
-              <button className="registration">Регистрация</button>
-            </div>
-          </form>
-        </div>
+          </li>
+        </ul>
       </div>
     </section>
   );
