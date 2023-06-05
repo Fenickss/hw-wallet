bf462e49bd2448480de93a1b488128bf88162d8b
Fenickss <denis.suxov.1991@mail.ru>
2023-06-05 15:40:44 +0300

    add

--- src/components/ShoppingListPage/ShoppingListPage.jsx
+++ src/components/ShoppingListPage/ShoppingListPage.jsx
@@ -59,17 +59,43 @@ const ShoppingListPage = () => {
                 </li>
               </ul>
             </div>
-            <ul className={s.currencies__list}>
-              <li className={s.currencies__item}>
-                <p className={s.currencies__text}>USD</p>
-              </li>
-              <li className={s.currencies__item}>
-                <p className={s.currencies__text}>EUR</p>
-              </li>
-              <li className={s.currencies__item}>
-                <p className={s.currencies__text}>RUB</p>
-              </li>
-            </ul>
+            <div className={s.priceConverter__main}>
+              <ul className={s.currencies__list}>
+                <li className={s.currencies__item}>
+                  <p className={s.currencies__text}>USD</p>
+                </li>
+                <li className={s.currencies__item}>
+                  <p className={s.currencies__text}>EUR</p>
+                </li>
+                <li className={s.currencies__item}>
+                  <p className={s.currencies__text}>RUB</p>
+                </li>
+              </ul>
+
+              <ul className={s.buyingСurrency__list}>
+                <li className={s.buyingСurrency__item}>
+                  <p className={s.buyingСurrency__text}>27.55</p>
+                </li>
+                <li className={s.buyingСurrency__item}>
+                  <p className={s.buyingСurrency__text}>30.00</p>
+                </li>
+                <li className={s.buyingСurrency__item}>
+                  <p className={s.buyingСurrency__text}>00.00</p>
+                </li>
+              </ul>
+
+              <ul className={s.saleСurrency__list}>
+                <li className={s.saleСurrency__item}>
+                  <p className={s.saleСurrency__text}>27.65</p>
+                </li>
+                <li className={s.saleСurrency__item}>
+                  <p className={s.saleСurrency__text}>30.10</p>
+                </li>
+                <li className={s.saleСurrency__item}>
+                  <p className={s.saleСurrency__text}>00.00</p>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </main>
