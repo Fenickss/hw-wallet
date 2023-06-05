2835bae28200544bbd86bae6ce0ff14d0eb55363
Fenickss <denis.suxov.1991@mail.ru>
2023-06-05 14:49:02 +0300

    add

--- src/components/ShoppingListPage/ShoppingListPage.jsx
+++ src/components/ShoppingListPage/ShoppingListPage.jsx
@@ -21,27 +21,58 @@ const ShoppingListPage = () => {
           </ul>
         </div>
       </div>
-      <div className={s.main}>
-        <nav className={s.nav}>
-          <ul className={s.nav__list}>
-            <li className={s.nav__item}>
-              <a className={s.nav__link} href="/">
-                Главная
-              </a>
-            </li>
-            <li className={s.nav__item}>
-              <a className={s.nav__link} href="/">
-                Статистика
-              </a>
-            </li>
-          </ul>
-        </nav>
+      <main className={s.main}>
+        <div className={s.nav__posicion}>
+          <nav className={s.nav}>
+            <ul className={s.nav__list}>
+              <li className={s.nav__item}>
+                <a className={s.nav__link} href="/">
+                  Главная
+                </a>
+              </li>
+              <li className={s.nav__item}>
+                <a className={s.nav__link} href="/">
+                  Статистика
+                </a>
+              </li>
+            </ul>
+          </nav>
 
-        <div className="balance">
-          <p>Ваш баланс</p>
-          <span>{'24000.00'}</span>
+          <div className={s.balance}>
+            <div className={s.balance__posicion}>
+              <p className={s.balance__text}>Ваш баланс</p>
+              <span className={s.balance__number}>{'24000.00'}</span>
+            </div>
+          </div>
+
+          <div className={s.priceConverter}>
+            <div className={s.priceConverter__header}>
+              <ul className={s.priceConverter__list}>
+                <li className={s.priceConverter__item}>
+                  <p className={s.priceConverter__title}>Валюта</p>
+                </li>
+                <li className={s.priceConverter__item}>
+                  <p className={s.priceConverter__title}>Покупка</p>
+                </li>
+                <li className={s.priceConverter__item}>
+                  <p className={s.priceConverter__title}>Продажа</p>
+                </li>
+              </ul>
+            </div>
+            <ul className={s.currencies__list}>
+              <li className={s.currencies__item}>
+                <p className={s.currencies__text}>USD</p>
+              </li>
+              <li className={s.currencies__item}>
+                <p className={s.currencies__text}>EUR</p>
+              </li>
+              <li className={s.currencies__item}>
+                <p className={s.currencies__text}>RUB</p>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
+      </main>
     </>
   );
 };
